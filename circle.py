b04a6ee917f8e917db893fd9fc1e09453f792acb
Gregory Tolmachev <GTollm@icloud.com>
2024-09-23 01:48:22 +0300

    Description added for functions in circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,25 @@ import math
 
 
 def area(r):
+    '''Counts the area of a circle using the radius.
+
+    Args:
+        r (int): radius f the circle.
+
+    Returns:
+        Area of the square.
+    '''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Counts the perimeter of a circle using radius.
+
+    Args:
+        a (int): radius of circle.
+
+    Returns:
+        Perimeter of circle.
+    '''
     return 2 * math.pi * r
 
