cf7f98a97d80a0ee36831facb3c5e535dde49342
Gregory Tolmachev <GTollm@icloud.com>
2024-09-23 01:51:14 +0300

    Description added for functions in triangle.py

--- triangle.py
+++ triangle.py
@@ -1,7 +1,26 @@
 def area(a, h):
+    '''Counts area of a triangle using one side and height of a triangle.
+
+    Args:
+        a (int): length of side of a triangle.
+        h (int): length of height of a triangle.
+
+    Returns:
+        Area of a triangle.
+    '''
     return a * h / 2
 
 def perimeter(a, b, c):
+    '''Counts perimeter of a triangle using it's three sides.
+
+    Args:
+        a (int): length of first side of a triangle.
+        b (int): length of second side of a triangle.
+        c (int): length of third side of a triangle.
+
+    Returns:
+        Perimeter of a triangle.
+    '''
     return a + b + c
 
 
