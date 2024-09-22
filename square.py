5778606bf9c7e0730e530c78d32e9b08c140a7f5
Gregory Tolmachev <GTollm@icloud.com>
2024-09-23 01:50:32 +0300

    Description added for functions in square.py

--- square.py
+++ square.py
@@ -1,7 +1,23 @@
 
 def area(a):
+    '''Counts the area of a square using the side length.
+
+    Args:
+      a (int): length of side of square.
+
+    Returns:
+      Area of the square.
+    '''
     return a * a
 
 
 def perimeter(a):
+    '''Counts the perimeter of a square using the side length.
+
+    Args:
+        a (int): length of side of square.
+
+    Returns:
+        Perimeter of the square.
+    '''
     return 4 * a
